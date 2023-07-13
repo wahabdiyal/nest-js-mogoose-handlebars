32c073654d0714eaae74e4efe28a310b99afb2c8
wahabdiyal <rana.wahab.146@gmail.com>
2023-07-14 00:50:16 +0500

    First Commit basic templete

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('pages/index.hbs')
+  root() {
+    return { message: "hello world!",
+              people: [
+                "Yehuda Katz",
+                "Alan Johnson",
+                "Charles Jolley",
+              ],
+              person: {
+                firstname: "Yehuda",
+                lastname: "Katz",
+              },
+              nav: [
+                { url: "http://www.yehudakatz.com", title: "Katz Got Your Tongue" },
+                { url: "http://www.sproutcore.com/block", title: "SproutCore Blog" }
+              ]
+        };
+  }
+}
